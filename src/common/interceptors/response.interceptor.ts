2dde790265236612140ef90557dc501f70e53841
Mohammad Alawad <engmohammadalawad@gmail.com>
2026-02-11 14:18:04 +0200

    feat(common): add response interceptor

---
+++ src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,32 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, map } from 'rxjs';
+
+interface ApiResponse<T> {
+  success: boolean;
+  timestamp: string;
+  data: T;
+}
+
+@Injectable()
+export class ResponseInterceptor<T> implements NestInterceptor<
+  T,
+  ApiResponse<T>
+> {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<T>,
+  ): Observable<ApiResponse<T>> {
+    return next.handle().pipe(
+      map((data: T) => ({
+        success: true,
+        timestamp: new Date().toISOString(),
+        data,
+      })),
+    );
+  }
+}
