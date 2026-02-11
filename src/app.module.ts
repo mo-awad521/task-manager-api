76b1e919b88063d17be50c981f16876360058fc7
Mohammad Alawad <engmohammadalawad@gmail.com>
2026-02-11 11:27:04 +0200

    refactor: add user module import to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { dataSourceOptions } from './database/typeorm.config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(dataSourceOptions),
     AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
